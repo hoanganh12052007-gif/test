fe8de548824c79d176ef1d0d70393f6ed467e902
Anh <hoanganh12052007@gmail.com>
2026-09-02 04:17:16 +0700

    updated

--- test/Program.cs
+++ test/Program.cs
@@ -1,4 +1,4 @@
-﻿student student1 = new student("John", 20, 1);
-student student2 = new student("Jane", 22, 2);
+﻿student student1 = new student("John", 20, 1, 3.5);
+student student2 = new student("Jane", 22, 2, 3.8);
 student1.DisplayInfo();
 student2.DisplayInfo();
--- test/student.cs
+++ test/student.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 class student
 {
-    public string name { get; set; } public int age { get; set; } public int id { get; set; }
-    public student(string name, int age, int id)
+    public string name { get; set; } public int age { get; set; } public int id { get; set; } public double gpa { get; set; }
+
+    public student(string name, int age, int id,double gpa)
     {
         this.name = name;
         this.age = age;
         this.id = id;
+        this.gpa = gpa;
+    }
+    public bool ispassed()
+    {
+        if (gpa > 2.0)
+            return true;
+        return false;
+    }
+    public string rank()
+    {
+        if(gpa >= 3.5)
+            return "A";
+        else if (gpa >= 3.0)
+            return "B";
+        else if (gpa >= 2.5)
+            return "C";
+        else if (gpa >= 2.0)
+            return "D";
+        else
+            return "F";
     }
     public void DisplayInfo()
     {
-        Console.WriteLine($"Name: {name}, Age: {age}, ID: {id}");
+        Console.WriteLine($"Name: {name}, Age: {age}, ID: {id}, GPA: {gpa}");
+        if(ispassed())
+            Console.WriteLine("The student has passed.");
+        else
+            Console.WriteLine("The student has failed.");
+        Console.WriteLine($"Rank: {rank()}");
     }
+   
 }
